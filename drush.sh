2d5a4f98b1ff53ef73660c8becfd38d206a0548d
Jay Beaton <jrb@jaybeaton.com>
2018-10-09 15:52:06 -0400

    use single-site drush script

--- drush.sh
+++ drush.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 
-SITE=$1
-
-if [[ $SITE == @* ]]; then
-    SITE=${SITE:1}
-fi
-
-# Remove first argument.
-shift;
-
-# Run drush command through docker.
-DRUSH_COMMAND="docker-compose exec php drush @local-$SITE $@";
+# Run Drush command through Docker.
+DRUSH_COMMAND="docker-compose exec php drush -r web/ $@";
 $DRUSH_COMMAND;
+
