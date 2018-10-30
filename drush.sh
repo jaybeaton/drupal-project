19babe0663679afcac187597fa74298f7a5f9ccb
Jay Beaton <jrb@jaybeaton.com>
2018-10-30 17:08:31 -0400

    update drush script

--- drush.sh
+++ drush.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Run Drush command through Docker.
-DRUSH_COMMAND="docker-compose exec php drush -r web/ $@";
+DRUSH_COMMAND="docker-compose exec php drush $@";
 $DRUSH_COMMAND;
 
