c984d1370a2b0523cf3e2aedccb27fb458d84a26
Jay Beaton <jrb@jaybeaton.com>
2020-05-05 18:41:08 -0400

    add lando support

--- drush.sh
+++ drush.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-# Run Drush command through Docker.
-DRUSH_COMMAND="docker-compose exec php drush $@";
-$DRUSH_COMMAND;
+if [ -f "./.use-docker-compose" ]; then
+    # Run Drush command through Docker.
+    DRUSH_COMMAND="docker-compose exec php drush $@";
+else
+    # Run Drush command through Lando.
+    DRUSH_COMMAND="lando drush $@";
+fi
 
+$DRUSH_COMMAND;
